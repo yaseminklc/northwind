07e7093e125b62300d7acc156478ef75fece2adc
yaseminklc <klcysmn0304@gmail.com>
2023-09-15 09:39:00 +0300

    2.gün (kullandığım APİ linki çalışmıyor) COMMİT1

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { HttpClient } from '@angular/common/http';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+  products: Product[] = [];
+  apiUrl = 'https://localhost:44314/api/products/getall';
+  constructor(private httpClient: HttpClient) {}
 
-  product1 = { productId: 1, productName: "Bardak", categoryId: 1, unitPrice: 5 }
-  product2 = { productId: 2, productName: "Laptop", categoryId: 1, unitPrice: 5 }
-  product3 = { productId: 3, productName: "Mouse", categoryId: 1, unitPrice: 5 }
-  product4 = { productId: 4, productName: "Keyboard", categoryId: 1, unitPrice: 5 }
-  product5 = { productId: 5, productName: "Camera", categoryId: 1, unitPrice: 5 }
-
-  products = [this.product1, this.product2, this.product3, this.product4, this.product5];
-
+  ngOnInit(): void {
+    this.getProducts();    
+  }
+  getProducts() {
+    this.httpClient
+      .get<ProductResponseModel>(this.apiUrl)
+      .subscribe((response) => {
+        this.products= response.data
+      });
+  }
 }
+ 
---
+++ src/app/models/product.ts
@@ -0,0 +1,10 @@
+export interface Product {
+    productId:number;
+    categoryId:number;
+    productName:string;
+    unitInStock:number;
+    unitPrice:number;
+
+}
+
+    
